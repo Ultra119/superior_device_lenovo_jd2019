e59972e05d6e7412a2461d6ba97b16c996bd1c62
Ultra119 <64977575+Ultra119@users.noreply.github.com>
2020-08-03 19:43:48 +0300

    jd2019: Superior Launcher

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
+	
+# Superior Launcher
+TARGET_LAUNCHER := oplauncher	
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/org.codeaurora.snapcam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.codeaurora.snapcam.xml
